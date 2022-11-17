ba478e12b00c60a6094d5e00205bcd9ceceaf762
Eveheeero <xhve00000@naver.com>
2022-11-17 23:10:48 +0900

    +

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(tree_sitter_toml::language()).unwrap();
+
+    let file = std::fs::read_to_string("./Cargo.toml").unwrap();
+    let parsed = parser.parse(&file, None).unwrap();
+
+    dbg!(parsed.root_node().child(1));
 }
